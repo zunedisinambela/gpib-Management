8040f47118a447fe01af529205ba7b084f7fd8e3
zunedisinambela <zunedisinambela@gmail.com>
2022-06-18 11:19:40 +0700

    Compute

--- gpib/models/family.py
+++ gpib/models/family.py
@@ -1,4 +1,6 @@
 from odoo import api, fields, models
+from dateutil.relativedelta import relativedelta
+from datetime import datetime, date
 
 
 class FamilyMaster(models.Model):
@@ -6,10 +8,14 @@ class FamilyMaster(models.Model):
     _description = "Family Master"
 
     name = fields.Char(string='Name', required=True)
-    qty = fields.Integer(string='Qty')
+    qty = fields.Integer(string='Qty', compute='_compute_family_line')
     address = fields.Text(string='Address', required=True)
     sector_id = fields.Many2one('gpib.sector', string='Name Sector', required=True)
     line_ids = fields.One2many('gpib.family.line', 'family_id')
+
+    def _compute_family_line(self):
+        for record in self:
+            record.qty = len(record.line_ids)
 
 
 class FamilyLine(models.Model):
@@ -22,26 +28,40 @@ class FamilyLine(models.Model):
     date_baptism = fields.Date(string='Date of Baptis')
     date_sidi = fields.Date(string='Date of Sidi')
     last_education = fields.Char(string='Last Education')
-    pelkat_id = fields.Many2one(string='Pelkat', required=True)
-    age = fields.Integer(string='Age')
+    pelkat_id = fields.Many2one(string='Pelkat')
+    age = fields.Integer(string='Age', compute='_compute_age')
     gender = fields.Selection([
         ('male', 'Male'),
         ('female', 'Female')
     ], required=True)
-    position = fields.Char(string='Position', required=True)
+    position = fields.Selection([
+        ('KK', 'Kepala Keluarga'),
+        ('IS', 'Istri'),
+        ('AN', 'Anak'),
+        ('OT', 'Orang Tua'),
+        ('FA', 'Family')
+    ], required=True)
     wedding_date = fields.Date(string='Wedding Date')
     state_married = fields.Selection([
         ('married', 'Married'),
         ('not_married', 'Not Married Yet'),
         ('divorced', 'Divorced'),
         ('death_divorce', 'Death Divorce')
-    ], required=True)
+    ], required=True, default='not_married')
     status = fields.Selection([
         ('active', 'Active'),
-        ('not_active', 'Not Active'),
-        ('die', 'Die')
-    ], required=True)
+        ('inactive', 'Inactive'),
+        ('died', 'Died')
+    ], required=True, default='active')
     change_church = fields.Char(string='Change Church')
     date_death = fields.Date(string='Date of Death')
     church_transfer_date = fields.Date(string='Church Transfer Date')
     church_move_out_date = fields.Date(string='Church Move Out Date')
+
+    def _compute_age(self):
+        for record in self:
+            if record.date_birthday:
+                dt = record.date_birthday
+                d2 = date.today()
+                rd = relativedelta(d2, dt)
+                record.age = rd.years
--- gpib/models/sector.py
+++ gpib/models/sector.py
@@ -6,4 +6,9 @@ class GpibSector(models.Model):
     _description = "App Sector"
 
     name = fields.Char(string='Name', required=True)
-    qty = fields.Integer(string='Qty')
+    qty = fields.Integer(string='Qty', compute='_compute_family')
+    family_ids = fields.One2many('gpib.family', 'sector_id')
+
+    def _compute_family(self):
+        for record in self:
+            record.qty = len(record.family_ids)
