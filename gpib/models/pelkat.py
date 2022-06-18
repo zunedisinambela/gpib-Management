c5f18e8575052e05ee77ac20386db22d09dfa28f
zunedisinambela <zunedisinambela@gmail.com>
2022-06-18 12:05:22 +0700

    Home Work

--- gpib/models/pelkat.py
+++ gpib/models/pelkat.py
@@ -6,3 +6,11 @@ class Pelkat(models.Model):
     _description = "App Pelkat"
 
     name = fields.Char(string='Name', required=True)
+    group_gender = fields.Selection([
+        ('all', 'All'),
+        ('male', 'Male'),
+        ('female', 'female'),
+    ], required=True, default='all')
+    is_married = fields.Boolean(default=False)
+    age_min = fields.Integer(default=0)
+    age_max = fields.Integer(default=60)
